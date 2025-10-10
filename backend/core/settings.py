fa4203ee85b1193dc9b057aec01be706e20e7795
ajoy <ajoygp551@gmail.com>
2025-10-10 07:21:32 +0530

    feat: settings - pagination

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # 3rd party
     "rest_framework",
+    "django_filters",
     # project
     "catalog",
     "shared",
@@ -137,4 +138,9 @@ ELASTICSEARCH_DSL = {
         "hosts": "http://elasticsearch:9200",
         # 'http_auth': ('username', 'password')
     }
+}
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
 }
