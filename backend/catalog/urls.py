9f2b1426bdb7fd7c939002a89ac20648e6d1daa3
ajoy <ajoygp551@gmail.com>
2025-10-12 19:13:28 +0530

    fix: fix url ordering

--- backend/catalog/urls.py
+++ backend/catalog/urls.py
@@ -5,8 +5,8 @@ from rest_framework.routers import DefaultRouter
 from .views import CatalogProductSearchViewset, ProductViewset, TestView
 
 router = DefaultRouter()
-router.register(r"", ProductViewset, basename="product")
 router.register(r"products", CatalogProductSearchViewset, basename="search")
+router.register(r"", ProductViewset, basename="product")
 urlpatterns = [
     path("test", TestView.as_view()),
 ]
